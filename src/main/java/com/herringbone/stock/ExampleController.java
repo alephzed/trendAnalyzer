6f290840e412f0aeb66a0002007ed379f4e9ba11
Adam Lee <aplee72@gmail.com>
2019-11-18 13:03:08 -0700

    Turn off controller

--- src/main/java/com/herringbone/stock/ExampleController.java
+++ src/main/java/com/herringbone/stock/ExampleController.java
@@ -9,13 +9,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+//@RequestMapping("/")
 public class ExampleController {
 
     @Autowired
